c67bf62c47277f916f6673335c5e7a278374b797
frnkthtnk101 <c0demonkers@gmail.com>
2021-05-01 09:32:23 -0400

    Update DeckWithJokers.cs
    Create code to create a deck of 54 and created the method that will shuffle the cards.

--- GameOfWAR/Logic/DeckWithJokers.cs
+++ GameOfWAR/Logic/DeckWithJokers.cs
@@ -6,15 +6,17 @@ using System.Threading.Tasks;
 using GameOfWAR.Interfaces;
 using GameOfWAR.POCOS;
 using GameOfWAR.Enums;
+using GameOfWAR.Helper;
 
 namespace GameOfWAR.Logic
 {
     public class DeckWithJokers : ICardhandler
     {
-        List<Card> DeckOfCards;
+        List<Card> _DeckOfCards;
+        readonly int _NumberOfCardsInDeck = 54;
         public DeckWithJokers()
         {
-            DeckOfCards = new List<Card>(54)
+            _DeckOfCards = new List<Card>(_NumberOfCardsInDeck)
             {
                 new Card()
                 {
@@ -22,7 +24,18 @@ namespace GameOfWAR.Logic
                     Value = CardValues.Joker
                 }
             };
-            foreach(var face in Enc)
+            var facesInDeck = EnumHelper.GetEnumList<CardFace>()
+                .Where(x => x != CardFace.Joker);
+            var valuesInDeck = EnumHelper.GetEnumList<CardValues>()
+                .Where(x => x != CardValues.Joker);
+            foreach (var face in facesInDeck)
+                foreach (var value in valuesInDeck)
+                    _DeckOfCards.Add(new Card()
+                    {
+                        Face = face,
+                        Value = value
+                    });
+
         }
         public void GetPlayerDeck(int Player)
         {
@@ -31,8 +44,16 @@ namespace GameOfWAR.Logic
 
         public void ShuffleCards()
         {
-            throw new NotImplementedException();
+            var indexScrambler = new Random();
+            for(int i = 0; i < _NumberOfCardsInDeck; i++)
+            {
+                var randomIndex = indexScrambler.Next(0, _NumberOfCardsInDeck - 1);
+                var tempvalue = _DeckOfCards[randomIndex];
+                _DeckOfCards[randomIndex] = _DeckOfCards[i];
+                _DeckOfCards[i] = tempvalue;
+            }
         }
+
 
         public void Split(int numOfPlayers = 2)
         {
